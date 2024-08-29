2a98d9db35920e1fe705087f294e256ac55ed140
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2024-08-29 17:01:19 +0200

    Add testing of the tarball.

--- test.sh
+++ test.sh
@@ -16,6 +16,7 @@ brew install python@3.12
 brew pkg --name python --with-deps --compress python@3.12
 test -f python.tgz
 test -f python.pkg
+# tar -ztvf python.tgz
 
 brew pkg --name python-without-deps --compress python@3.12
 test -f python-without-deps.tgz
@@ -26,6 +27,10 @@ brew install ruby@3.3
 brew pkg --name ruby-with-python --compress --additional-deps python@3.12 ruby@3.3
 test -f ruby-with-python.tgz
 test -f ruby-with-python.pkg
-tar -ztvf ruby-with-python.tgz
+# tar -ztvf ruby-with-python.tgz
 
+# Debug files and sizes
 ls -lh
+
+# TODO: Change this into master: https://github.com/metacall/homebrew-brew-pkg/blob/5a39b11089b479c3513d290c8aeb836ff989a5f3/brew-pkg.rb#L4
+# And leave the develop line commented for testing out
