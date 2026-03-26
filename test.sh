0d8323bd3cc405f3dbf0cc34080a83829c86cc9f
Vicente Eduardo Ferrer Garcia <7854099+viferga@users.noreply.github.com>
2026-03-26 03:57:21 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,30 +1,39 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+# Install latest brew
 if [[ $(command -v brew) == "" ]]; then
     echo "Installing brew in order to build MetaCall"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+# Install brew-pkg
 brew tap --verbose metacall/brew-pkg
 brew install --verbose --HEAD metacall/brew-pkg/brew-pkg
 
+# Test Python with dependencies, compress and custom output tarball name
 brew install python@3.12
 brew pkg --name python --with-deps --compress python@3.12
 test -f python.tar.gz
 test -f python.pkg
+tar -ztvf python.tar.gz
 
+# Test Python without dependencies, compress and custom output tarball name
 brew pkg --name python-without-deps --compress python@3.12
 test -f python-without-deps.tar.gz
 test -f python-without-deps.pkg
+tar -ztvf python-without-deps.tar.gz
 
+# Test Ruby with additional dependencies, compress and custom output tarball name
 brew install ruby@3.3
 brew pkg --name ruby-with-python --compress --relocatable --additional-deps python@3.12 ruby@3.3
 test -f ruby-with-python.tar.gz
 test -f ruby-with-python.pkg
+tar -ztvf ruby-with-python.tar.gz
 
 ls -lh python.tar.gz python.pkg python-without-deps.tar.gz python-without-deps.pkg ruby-with-python.tar.gz ruby-with-python.pkg
 
+# Symlink tests
 verify_symlinks() {
     local tarball=$1
     local extract_dir="/tmp/verify_$$"
